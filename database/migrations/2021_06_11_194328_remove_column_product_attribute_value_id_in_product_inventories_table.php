baecc589632af6ead6e671676851eb64ae9942ee
Muhammad Fajar <muhammadfajar191@gmail.com>
2021-06-12 02:56:41 +0700

    update 10

--- database/migrations/2021_06_11_194328_remove_column_product_attribute_value_id_in_product_inventories_table.php
+++ database/migrations/2021_06_11_194328_remove_column_product_attribute_value_id_in_product_inventories_table.php
@@ -6,27 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 class RemoveColumnProductAttributeValueIdInProductInventoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('product_inventories', function (Blueprint $table) {
-            //
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('product_inventories', function (Blueprint $table) {
+      $table->dropForeign(['product_attribute_value_id']);
+      $table->dropColumn('product_attribute_value_id');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('product_inventories', function (Blueprint $table) {
-            //
-        });
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('product_inventories', function (Blueprint $table) {
+      $table->unsignedBigInteger('product_attribute_value_id');
+      $table->foreign('product_attribute_value_id')->references('id')->on('product_attribute_values')->onDelete('cascade');
+    });
+  }
 }
