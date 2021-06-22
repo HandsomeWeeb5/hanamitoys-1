2ad70cccbc1b6ab176f918397385d1aeed29ee60
Muhammad Fajar <muhammadfajar191@gmail.com>
2021-06-23 02:41:26 +0700

    update 16

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -58,7 +58,7 @@
             @foreach ($users as $user)
             <tr>
               <td>{{ $user->id }}</td>
-              <td>{{ $user->name }}</td>
+              <td>{{ $user->first_name }} {{ $user->last_name }}</td>
               <td>{{ $user->email }}</td>
               <td>{{ $user->roles->implode('name', ', ') }}</td>
               <td>{{ $user->created_at }}</td>
